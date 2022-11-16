772dadf96040d83dddccc6e047d9f6c6b441e968
Aleksandr Solovev <zuxelusa@ya.ru>
2022-11-16 15:07:17 +0300

    changed the functionsd

--- action.py
+++ action.py
@@ -1,19 +1,19 @@
-def read_base(file):
-    print('Чтение')
-    with open(file, 'r', encoding='utf-8') as text:
-        last_id=int(text.readline().split('\n')[0])
-        resultData = list()
-        dict_contact={}
-        dict_phone={}
-        for line in text.readlines():
-            list_record=line.split('\n')[0].split(';')
-            dict_contact[int(list_record[0])]=list_record[1]
-            dict_phone[int(list_record[0])]=list_record[2]
-        resultData.append(dict_contact)
-        resultData.append(dict_phone)
-    return resultData, last_id
+# def read_base(file):
+#     print('Чтение')
+#     with open(file, 'r', encoding='utf-8') as text:
+#         last_id=int(text.readline().split('\n')[0])
+#         resultData = list()
+#         dict_contact={}
+#         dict_phone={}
+#         for line in text.readlines():
+#             list_record=line.split('\n')[0].split(';')
+#             dict_contact[int(list_record[0])]=list_record[1]
+#             dict_phone[int(list_record[0])]=list_record[2]
+#         resultData.append(dict_contact)
+#         resultData.append(dict_phone)
+#     return resultData, last_id
 
-def read_base_(file):
+def read_base(file):
     print('Чтение')
     with open(file, 'r', encoding='utf-8') as text:
         # last_id=int(text.readline().split('\n')[0])
@@ -27,15 +27,23 @@ def read_base_(file):
         resultData.append(dict_contact)
         resultData.append(dict_phone)
     return resultData
-def last_baseid(result):
-    return max(list(result[0].keys()))
 
-print(read_base_("phone_.txt"))
-print(last_baseid(read_base_("phone_.txt")))
+# def last_baseid(result):
+#     return max(list(result[0].keys()))
+
+# print(read_base_("phone_.txt"))
+# print(last_baseid(read_base_("phone_.txt")))
+
+# def save_base(file,base,last_id):
+#     print('Запись')
+#     with open(file, 'w', encoding='utf-8') as text:
+#         text.write(str(last_id)+'\n')
+#         for key in base[0].keys():
+#             text.write(str(key)+';'+base[0][key]+';'+base[1][key]+'\n')
 
 
 
-def save_base(file,base,last_id):
+def save_base_(file,base,last_id):
     print('Запись')
     with open(file, 'w', encoding='utf-8') as text:
         text.write(str(last_id)+'\n')
