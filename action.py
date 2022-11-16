066d5226b3f42b784fd579bb4ef42dd596d2d38b
Aleksandr Solovev <zuxelusa@ya.ru>
2022-11-16 15:26:42 +0300

    action edited and phone.txt edited

--- action.py
+++ action.py
@@ -13,7 +13,7 @@
 #         resultData.append(dict_phone)
 #     return resultData, last_id
 
-def read_base(file):
+def read_base(file): # чтение содержимого справочника. возвращает кортеж двух словарей (fio и phones)
     print('Чтение')
     with open(file, 'r', encoding='utf-8') as text:
         # last_id=int(text.readline().split('\n')[0])
@@ -28,8 +28,8 @@ def read_base(file):
         resultData.append(dict_phone)
     return resultData
 
-# def last_baseid(result):
-#     return max(list(result[0].keys()))
+def last_baseid(result): # определяет последний id в выгруженном справочнике
+    return max(list(result[0].keys()))
 
 # print(read_base_("phone_.txt"))
 # print(last_baseid(read_base_("phone_.txt")))
@@ -41,14 +41,13 @@ def read_base(file):
 #         for key in base[0].keys():
 #             text.write(str(key)+';'+base[0][key]+';'+base[1][key]+'\n')
 
-
-
-def save_base_(file,base,last_id):
+def new_contact(file,base,last_id):
     print('Запись')
-    with open(file, 'w', encoding='utf-8') as text:
-        text.write(str(last_id)+'\n')
-        for key in base[0].keys():
-            text.write(str(key)+';'+base[0][key]+';'+base[1][key]+'\n')
+    with open(file, 'a', encoding='utf-8') as text:
+        text.write(str(last_id) + ';' + base[0] + ';' + base[1] + '\n')
+
+result = read_base("phone.txt")
+save_base("phone.txt", ("ИИИ", "099090283"), last_baseid(result))
 
 def find_contact():
     print('Поиск')
