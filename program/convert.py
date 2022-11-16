59ce844c74959f5056d77b10dd90e1df1553499d
Aleksandr Solovev <zuxelusa@ya.ru>
2022-11-16 21:13:50 +0300

    modified converters. NEED MORE CORRECTION

--- program/convert.py
+++ program/convert.py
@@ -10,7 +10,7 @@ from convert_rules import HTML
 
 def convert_to_csv(sou_file):
     f_s = open(sou_file, "r")
-    res_file = sou_file.split('.')[0] + ".csv"
+    res_file = sou_file.split('.')[-1] + ".csv"
     f_r = open(res_file, "w")
     while True:
         current = f_s.readline()
@@ -22,9 +22,10 @@ def convert_to_csv(sou_file):
 
 def convert_to_html(sou_file):
     f_s = open(sou_file, "r")
-    res_file = sou_file.split('.')[0] + ".html"
+    res_file = sou_file.split('.')[-1] + ".html"
     f_r = open(res_file, "w")
-    f_r.writelines("<html>\n<head></head>\n<body>\n<table>\n")
+    f_r.writelines("<html>\n<head>PHONEBOOK</head>\n<body>\n<table>\n")
+    f_r.writelines("<th><td>ID</td><td>NAME</td><td>PHONES</td></th>")
     while True:
         current = f_s.readline()
         if not current:
@@ -33,3 +34,6 @@ def convert_to_html(sou_file):
     f_r.writelines("</table>\n</body>")
     f_s.close()
     f_r.close()
+
+convert_to_html("..\phone.txt")
+convert_to_csv("..\phone.txt")
