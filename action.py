05484029bf84d52ed97ca42999fed8337987e910
Aleksandr Solovev <zuxelusa@ya.ru>
2022-11-16 15:00:40 +0300

    modified read_base and create last id

--- action.py
+++ action.py
@@ -13,6 +13,28 @@ def read_base(file):
         resultData.append(dict_phone)
     return resultData, last_id
 
+def read_base_(file):
+    print('Чтение')
+    with open(file, 'r', encoding='utf-8') as text:
+        # last_id=int(text.readline().split('\n')[0])
+        resultData = list()
+        dict_contact={}
+        dict_phone={}
+        for line in text.readlines():
+            list_record=line.split('\n')[0].split(';')
+            dict_contact[int(list_record[0])]=list_record[1]
+            dict_phone[int(list_record[0])]=list_record[2]
+        resultData.append(dict_contact)
+        resultData.append(dict_phone)
+    return resultData
+def last_baseid(result):
+    return max(list(result[0].keys()))
+
+print(read_base_("phone_.txt"))
+print(last_baseid(read_base_("phone_.txt")))
+
+
+
 def save_base(file,base,last_id):
     print('Запись')
     with open(file, 'w', encoding='utf-8') as text:
